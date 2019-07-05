fba03cfc167b0e812c60c5109d5ae1b1d37751f9
fardinDev <phpfardin@gmail.com>
2019-07-06 00:15:23 +0600

    category tree view on admin panel

---
+++ resources/views/cat.blade.php
@@ -0,0 +1,25 @@
+
+@foreach($childs as $child)
+<ol class="dd-list">
+
+
+    <li class="dd-item" data-id="1">
+  
+    <div class="pull-right item_actions">
+
+        <a href="{{url('admin/product-categories/'.$child->id)}}" title="View" class="btn btn-sm btn-warning pull-right view">
+        <i class="voyager-eye"></i> <span class="hidden-xs hidden-sm">View</span>
+        </a>
+    </div>
+    <div class="dd-handle"  style="{{$sub == 1 ? '' : 'background-color:#c0c2c2'}}">
+        <span  style="color:black">{{$child->name}}</span>
+    </div>
+    </li>
+
+    @if(count($child->childs))
+    @include('cat',['childs' => $child->childs, 'sub' => 1])
+    @endif
+
+</ol>
+@endforeach
+
